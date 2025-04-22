965e74a6201c72de7cd1c2aa05239359e804b6b9
AniketBhudke <bhudkea@gmail.com>
2025-04-22 23:20:34 +0530

    2nd day

---
+++ Recursion/o to hero.py
@@ -0,0 +1,61 @@
+#addition
+# def addition(a,b):
+#     if a==0:
+#         return b
+#     else:
+#         return addition(a-1,b+1)
+# print(addition(4,5))    
+
+#substraction
+# def sub(a,b):
+#     if b==0:
+#         return a
+#     else:
+#         return sub(a-1,b-1)
+# print(sub(4,6))    
+
+
+#multiplication
+# def mulplication(a,b):
+#     if b==1:
+#         return a
+#     else:
+#         return a+mulplication(a,b-1)
+# print(mulplication(6,9))
+
+#division
+# def division(a,b):
+#     if a==0:
+#         return 0
+#     else:
+#         return 1+division(a-b,b)
+# print(division(1000,2))    
+
+# #length
+# def recursive_length(lst):
+#     if lst == []:
+#         return 0
+#     else:
+#         return 1 + recursive_length(lst[1:])
+# print(recursive_length([11,2,3,4,55,6,7,8,90]))
+
+#count
+# def count_occurrences(lst, target):
+#     if not lst:
+#         return 0
+#     if lst[0] == target:
+#         return 1 + count_occurrences(lst[1:], target)
+#     else:
+#         return count_occurrences(lst[1:], target)
+# print(count_occurrences([1,22,3,4,5,66,7,8,9,90,1,1],1))
+
+
+#find dupliacte number
+# Input: [3, 1, 3, 4, 2]
+# Output: 3
+def duplicatenumber(lst):
+    if lst==[]:
+        return 0
+    elif:
+        
+
