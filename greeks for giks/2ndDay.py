77b6122ce5a9cfaf6f325476dfdfb5e36adad559
AniketBhudke <bhudkea@gmail.com>
2025-04-22 23:36:23 +0530

    2nd day

--- greeks for giks/2ndDay.py
+++ greeks for giks/2ndDay.py
@@ -1,10 +1,20 @@
-def subarraySum( arr, target):
-        # code here
-        for i in range(len(arr)):
-            for j in range(i+1,len(arr)):
-                if arr[i]+arr[j]==target:
-                    return [i,j]
-        return [-1]
-arr=[1,22,3,4,5,66,77,8]
-target=88
-print(subarraySum( arr, target))
+def subarraySum(arr, n, target):
+    start = 0
+    curr_sum = 0
+
+    for end in range(n):
+        curr_sum += arr[end]
+
+        while curr_sum > target and start < end:
+            curr_sum -= arr[start]
+            start += 1
+
+        if curr_sum == target:
+            return [start + 1, end + 1]  # 1-based indexing
+
+    return [-1]
+arr=[11,2,3,4,5,6,7,8]
+n=3
+target=9
+print(subarraySum(arr, n, target)
+)
